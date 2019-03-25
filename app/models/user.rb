51748bac563eafc8a17d05000cf22a2ae4da6adb
Milos Vukadinovic <milos.vukadinovic11@gmail.com>
2019-03-25 17:27:32 +0100

    additional changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
 
     validates :username, presence: true,uniqueness: true,length:{minimum:3,maximum:16}
     validates :email, presence: true,uniqueness: true
-    validates :password, presence: true,length:{minimum:3,maximum:16}
-    validates :password, :confirmation => true
+    validates :password, presence: true,length:{minimum:3,maximum:16},:confirmation => true
 end
 
